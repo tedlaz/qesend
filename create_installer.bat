fc844a96c6cec6cff6d8ea44af4e8b3527bb37ac
Ted Lazaros <tedlaz@gmail.com>
2023-03-29 10:27:14 +0300

    minor fixes

--- create_installer.bat
+++ create_installer.bat
@@ -1,5 +1,5 @@
 pyinstaller -w qesend.py
-DEL C:\Users\tedla\prj\qesend\dist\qesend\PyQt6\Qt6\bin\opengl32sw.dll
+DEL .\dist\qesend\PyQt6\Qt6\bin\opengl32sw.dll
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setup_script.iss
 rmdir /s /q build
 rmdir /s /q dist
